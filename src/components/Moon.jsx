82cb228958f25d0499a13e8ad07c83f151ddcd0d
Irfan Ashraf <realirfanashraf@gmail.com>
2024-09-06 00:58:12 +0530

    added motion to the moon

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import {motion} from 'framer-motion'
 const Moon = () => {
   return (
-    <div>Moon</div>
+    <motion.div
+      className='w-32 h-32 rounded-full bg-slate-300'
+      initial={{ x: 0 }} 
+      animate={{ x: -100 }} 
+      transition={{ duration: 2 }} 
+    >
+    </motion.div>
+
+    
   )
 }
 
--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Sun = () => {
   return (
-    <div>Sun</div>
-  )
+    <div className='w-32 h-32 bg-yellow-200 rounded-full'>
+    </div>
+)
 }
 
 export default Sun
